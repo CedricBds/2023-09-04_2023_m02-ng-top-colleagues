8aebce1ba49fe09f88b03e4a2c24caffe484eb18
Cédric <86633249+CedricBds@users.noreply.github.com>
2023-09-05 17:04:04 +0200

    feat: colleaguelist component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LikeHateComponent } from '../shared/components/like-hate/like-hate.component';
 import { ColleagueComponent } from '../shared/components/colleague/colleague.component';
+import { ColleagueListComponent } from '../shared/components/colleague-list/colleague-list.component';
 
 @NgModule({
-  declarations: [LikeHateComponent, ColleagueComponent],
+  declarations: [LikeHateComponent, ColleagueComponent, ColleagueListComponent],
   imports: [CommonModule],
-  exports: [LikeHateComponent, ColleagueComponent],
+  exports: [LikeHateComponent, ColleagueComponent, ColleagueListComponent],
 })
 export class SharedModule {
   
