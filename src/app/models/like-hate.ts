1ddc9f94277fe1a32986a2ed9d59d78e71dc42bf
Cédric <86633249+CedricBds@users.noreply.github.com>
2023-09-05 16:22:16 +0200

    wip incrementation

--- src/app/models/like-hate.ts
+++ src/app/models/like-hate.ts
@@ -1,2 +1,4 @@
 export enum LikeHate {
+    LIKE,
+    HATE
 }
--- src/app/shared/components/like-hate/like-hate.component.ts
+++ src/app/shared/components/like-hate/like-hate.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { LikeHate } from 'src/app/models/like-hate';
 
 @Component({
   selector: 'tc-like-hate',
   templateUrl: './like-hate.component.html',
-  styleUrls: ['./like-hate.component.scss']
+  styleUrls: ['./like-hate.component.scss'],
 })
 export class LikeHateComponent {
+  @Output() score: EventEmitter<LikeHate> = new EventEmitter<LikeHate>();
 
+  addScore() {
+    this.score.emit(LikeHate.LIKE)
+  }
+
+  removeScore() {
+    this.score.emit(LikeHate.HATE)
+  }
+  
 }
